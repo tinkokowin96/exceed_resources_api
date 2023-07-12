11f31c7eecbc483cd52e52315c695b2d07ce19e6
Tin Ko Ko Win <tinkokowin@ucsy.edu.mm>
2023-07-12 15:02:05 +0630

    scheduled task schema

---
+++ src/scheduled_tasks/scheduled_tasks.schema.ts
@@ -0,0 +1,26 @@
+import { Prop } from '@nestjs/mongoose';
+import { IsDateString, IsEnum, IsNotEmpty } from 'class-validator';
+import { SchemaTypes } from 'mongoose';
+import { CoreSchema } from 'src/common/schema/core.shema';
+import { EScheduledTask } from 'src/common/util/enumn';
+import { User } from 'src/user/schema/user.schema';
+
+export class ScheduledTask extends CoreSchema {
+  @Prop({ type: Date, required: true })
+  @IsNotEmpty()
+  @IsDateString()
+  executionTime: Date;
+
+  @Prop({ type: String, enum: EScheduledTask })
+  @IsNotEmpty()
+  @IsEnum(EScheduledTask)
+  type: EScheduledTask;
+
+  @Prop({ type: SchemaTypes.Mixed, required: true })
+  @IsNotEmpty()
+  payload: any;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'User' })
+  @IsNotEmpty()
+  user: User;
+}
